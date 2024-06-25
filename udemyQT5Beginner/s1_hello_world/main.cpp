a643ae01f964fa54ad039fa1834487896902ff46
Engin Gedik <enggdk13@gmail.com>
2024-06-25 18:18:17 +0300

    using self documenting annotations, works with doxygen

--- udemyQT5Beginner/s1_hello_world/main.cpp
+++ udemyQT5Beginner/s1_hello_world/main.cpp
@@ -2,6 +2,13 @@
 #include <QDebug>
 // imports the qt debugging lib
 
+/**
+ * @brief main - name of function being referred to // starting point
+ * @param argc - the argument count
+ * @param argv - the arguments value
+ * @return -integer type,  the exit code/value of the app
+ */
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv); // creates a QT core app
